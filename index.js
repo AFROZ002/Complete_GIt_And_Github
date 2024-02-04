a13b0906c9f24775278394d2839f940ee1262569
Afroz Alam <afrozcpr@gmail.com>
2024-02-05 01:25:31 +0530

    Remove the 18years Functions

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ console.log("Afroz Alam")
 const age=24
 const Date ='05/02/2024'
 
-function ageisAbove18(){
-    return age>=18
-    
-}
-
+ 
 function Myname(){
    console.log('Say my Name'+name);
     
