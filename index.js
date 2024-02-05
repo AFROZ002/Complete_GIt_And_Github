1300547c5993abbeec37f48a53732fbc1601e6a8
Afroz Alam <afrozcpr@gmail.com>
2024-02-05 22:11:07 +0530

    Added a New Functio

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function Myname(){
    console.log('Say my Name'+name);
     
 }
+function newfn(){
+   console.log("hey Iam new Function");
+
+}
+
+function MaheshDalee(){
+    console.log("Hii iam Mahesh Dalee From Aptio Company");
+
+}
